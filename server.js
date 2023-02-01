ed68dfc98a51cee4d50a1c870045fe025b132c63
CarThrottlefan <alexandervici@gmail.com>
2023-02-01 11:45:02 +0100

    All of the functions work, added a bonus delete function that resets the database to it's initial values

--- server.js
+++ server.js
@@ -26,6 +26,7 @@
 
 const sqlite = require('sqlite3').verbose();
 let db = my_database('./gallery.db');
+let deleteCheck = false;
 
 // ###############################################################################
 // The database should be OK by now. Let's setup the Web server so we can start
@@ -43,13 +44,21 @@ app.use(express.json());
 // ###############################################################################
 // Routes
 // 
-// TODO: Add your routes here and remove the example routes once you know how
-//       everything works.
 
-app.put("/currDb/item/:uid", function(req, res){ //TODO ask if I can use a patch method, instead of PUT. IF so, remove the checks for null, since a patch method only changes parts of an entry. (in theory)
 
+//---------------------A simple test to see if the server is operational(left this in on purpose) ------------
+// This example route responds to http://localhost:3000/hello with an example JSON object.
+app.get("/hello", function(req, res) {
+	response_body = {'Hello': 'World'} ;
+
+	// This example returns valid JSON in the response, but does not yet set the
+	// associated HTTP response header. 
+	res.json(response_body) ;
+});
+
+//-----------------------Update functionality -----------------
+app.put("/currDb/item/:uid", function(req, res){
 	idReq = req.params.uid;
-	console.log(idReq + ' id of curr');
 	item = req.body;
 	let fail = false;
 	let errType = '';
@@ -61,7 +70,8 @@ app.put("/currDb/item/:uid", function(req, res){ //TODO ask if I can use a patch
 		if (!row['EXISTS(SELECT 1 FROM gallery WHERE id=?)']) {
 		  res.status(404);
 		  res.json('Error - item with the given ID does not exist');
-		} else 
+		} 
+		else 
 		{
 			db.run(`UPDATE gallery
 				SET author=?, alt=?, tags=?, image=?, description=? WHERE id=?`,
@@ -108,15 +118,16 @@ app.put("/currDb/item/:uid", function(req, res){ //TODO ask if I can use a patch
 								console.log('Malformed user input, missing' + errType);
 							}
 							else{
-								res.status(200);
-								res.json('OK'); 
+								res.status(203);
+								res.json('Update completed successfully'); 
 							}
 				});			
 		}
 	  });	
 });
 
-app.post("/currDb/insert", function(req, res){
+//---------------------Inserting a new entry in the database --------------------
+app.post("/currDb", function(req, res){
 	item = req.body;
 	let fail = false;
 	let errType = '';
@@ -162,85 +173,117 @@ app.post("/currDb/insert", function(req, res){
 						console.log('Malformed user input, missing' + errType);
 					}
 					else{
-						res.status(200);
-						res.json('OK'); 
+						res.status(202);
+						res.json('Inserted entry in database'); 
 					}
 				});			
 });
 // ###############################################################################
 
-// This example route responds to http://localhost:3000/hello with an example JSON object.
-// Please test if this works on your own device before you make any changes.
 
-app.get("/hello", function(req, res) {
-    response_body = {'Hello': 'World'} ;
 
-    // This example returns valid JSON in the response, but does not yet set the
-    // associated HTTP response header.  This you should do yourself in your
-    // own routes!
-    res.json(response_body) ;
-});
-
-// This route responds to http://localhost:3000/db-example by selecting some data from the
-// database and return it as JSON object.
-// Please test if this works on your own device before you make any changes.
-/*app.get('/db-example', function(req, res) {
-    // Example SQL statement to select the name of all products from a specific brand
-	db.all(`SELECT * FROM gallery WHERE author=?`, ['Grace Hopper'], function(err, rows) {
-	
-    	// TODO: add code that checks for errors so you know what went wrong if anything went wrong
-    	// TODO: set the appropriate HTTP response headers and HTTP response codes here.
-
-    	// # Return db response as JSON
-    	return res.json(rows);
-    });
-});*/
-
-app.get('/currDb', function(req, res) {
+//------------Retrieves the whole database ------------------------
+app.get('/currDb', function(req, res) { 
 	
 	db.all("SELECT * FROM gallery", (err, rows) => {
-		res.set('Content-Type', 'application/json');
-		res.status(200);
-		
 		if(err)
 		{
 			res.status(404);
 			console.error(err.message);
 		}
+		else
+		{
+			res.set('Content-Type', 'application/json');
+			res.status(200);
+			return res.json(rows);
+		}
 		
-		return res.json(rows);
 	});
 });
 
-app.get('/currDb/item/:uid', function(req, res) {
-	let id = req.params.uid;
-	db.all("SELECT author, alt, tags, image, description FROM gallery WHERE id=" + id, (err, rows) => {
+//--------------Retrieves a single item, based on the query id ----------------------
+app.get('/currDb/item/:id', function(req, res) { 
+	let id = req.params.id;
+	db.all(`SELECT * FROM gallery WHERE id = ?`, [id], (err, rows) => {
 		
-		res.set('Content-Type', 'application/json');
-		res.status(200);
-
-		if(!db.all('SELECT id FROM gallery WHERE id=' + id))
-		{
-			console.log('err found');
-			err = 'Not found';
-		}
-
 		if(err)
 		{
 			res.status(404);
-			console.error(err.message);
+			res.json('Not found');
 		}
-		
-		return res.json(rows);
+		else if (rows.length === 0)
+		{
+			res.status(404);
+			res.json('Not found');
+		}
+		else
+		{
+			res.set('Content-Type', 'application/json');
+			res.status(201);
+			return res.json(rows);
+		}
 	});
 });
 
-
-app.post('/post-example', function(req, res) {
-	// This is just to check if there is any data posted in the body of the HTTP request:
-	console.log(req.body);
-	return res.json(req.body);
+//---------------Deletes a single entry in the db --------------------------
+app.delete("/currDb/item/:id", function(req, res) {
+	db.run(`DELETE FROM gallery WHERE id = ?`, [req.params.id], (err) => {
+	  if (err) {
+		res.status(500);
+		res.json(err.message);
+	   } 
+		else {
+		db.get(`SELECT * FROM gallery WHERE id = ?`, [req.params.id], (err, row) => {
+		  if(err)
+		  {
+			res.status(500);
+			res.json(err.message);
+		  }
+		else if (!row) {
+			res.status(404);
+			res.json("Not Found");
+		  	}
+		else
+		{
+			res.status(204);
+			res.json("Item Deleted");
+		}
+	});
+}
 });
+});
+
+
+app.delete("/currDb", function(req, res){
+	
+	db.run(`DELETE FROM gallery`, (err) => {
+		if(err) {
+			res.status(500);
+			res.json(err.message);
+		}
+		else {
+			db.all("SELECT * FROM gallery", (err, rows) => {
+				if(err)
+				{
+					res.status(404);
+					console.error(err.message);
+				}
+				if(rows.length > 0)
+				{
+					res.status(400);
+					return res.json('Database not deleted');
+				}
+				else
+				{
+					recreateDatabase(true);
+					res.status(205);
+					return res.json('Database deleted successfully. Reinitialized default values');
+				}
+			});
+		}
+	});
+});
+
 
 
 // ###############################################################################
@@ -255,47 +298,57 @@ function my_database(filename) {
 	// Conncect to db by opening filename, create filename if it does not exist:
 	let i; //TODO see if needed
 	var db = new sqlite.Database(filename, (err) => {
-  		if (err) {
+		if (err) {
 			console.error(err.message);
-  		}
-  		console.log('Connected to the phones database.');
+		}
+		console.log('Connected to the database.');
 	});
 	// Create our phones table if it does not exist already:
 	db.serialize(() => {
 		db.run(`
-        	CREATE TABLE IF NOT EXISTS gallery
-        	 (
-                    id INTEGER PRIMARY KEY,
-                    author CHAR(100) NOT NULL,
-                    alt CHAR(100) NOT NULL,
-                    tags CHAR(256) NOT NULL,
-                    image char(2048) NOT NULL,
-                    description CHAR(1024) NOT NULL
-		 )
-		`);
-		db.all(`select count(*) as count from gallery`, function(err, result) {
-			if (result[0].count == 0) {
-				db.run(`INSERT INTO gallery (author, alt, tags, image, description) VALUES (?, ?, ?, ?, ?)`, [
-        			"Tim Berners-Lee",
-        			"Image of Berners-Lee",
-        			"html,http,url,cern,mit",
-        			"https://upload.wikimedia.org/wikipedia/commons/9/9d/Sir_Tim_Berners-Lee.jpg",
-        			"The internet and the Web aren't the same thing."
+		CREATE TABLE IF NOT EXISTS gallery
+		(
+			id INTEGER PRIMARY KEY,
+			author CHAR(100) NOT NULL,
+			alt CHAR(100) NOT NULL,
+			tags CHAR(256) NOT NULL,
+			image char(2048) NOT NULL,
+			description CHAR(1024) NOT NULL
+			)
+			`);
+			db.all(`select count(*) as count from gallery`, function(err, result) {
+				if (result[0].count == 0) {
+					db.run(`INSERT INTO gallery (author, alt, tags, image, description) VALUES (?, ?, ?, ?, ?)`, [
+						"Tim Berners-Lee",
+						"Image of Berners-Lee",
+						"html,http,url,cern,mit",
+						"https://upload.wikimedia.org/wikipedia/commons/9/9d/Sir_Tim_Berners-Lee.jpg",
+						"The internet and the Web aren't the same thing."
     				]);
-				db.run(`INSERT INTO gallery (author, alt, tags, image, description) VALUES (?, ?, ?, ?, ?)`, [
-        			"Grace Hopper",
-        			"Image of Grace Hopper at the UNIVAC I console",
-        			"programming,linking,navy",
-        			"https://upload.wikimedia.org/wikipedia/commons/3/37/Grace_Hopper_and_UNIVAC.jpg",
-				"Grace was very curious as a child; this was a lifelong trait. At the age of seven, she decided to determine how an alarm clock worked and dismantled seven alarm clocks before her mother realized what she was doing (she was then limited to one clock)."
+					db.run(`INSERT INTO gallery (author, alt, tags, image, description) VALUES (?, ?, ?, ?, ?)`, [
+						"Grace Hopper",
+						"Image of Grace Hopper at the UNIVAC I console",
+						"programming,linking,navy",
+						"https://upload.wikimedia.org/wikipedia/commons/3/37/Grace_Hopper_and_UNIVAC.jpg",
+						"Grace was very curious as a child; this was a lifelong trait. At the age of seven, she decided to determine how an alarm clock worked and dismantled seven alarm clocks before her mother realized what she was doing (she was then limited to one clock)."
     				]);
-				console.log('Inserted dummy photo entry into empty database');
-			} else {
-				//i = result[0].count;
-				console.log("Database contains", result[0].count, "item(s) at startup.");
-			}
+					console.log('Inserted dummy photo entry into empty database');
+				} else {
+					//i = result[0].count;
+					console.log("Database contains", result[0].count, "item(s) at startup.");
+				}
+			});
+			
 		});
-
-	});
+		return db;
+	}
+	
+function recreateDatabase(deleteCheck)
+{
+	if(deleteCheck)
+	{
+		db = my_database('./gallery.db');
+	}
 	return db;
 }
+	
